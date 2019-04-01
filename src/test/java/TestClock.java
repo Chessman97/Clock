7081e5ba2302140acae8fd4223daa03f19930027
Chessman97 <47553161+Chessman97@users.noreply.github.com>
2019-04-01 11:51:43 +0600

    Тест на получение времени будильника

--- src/test/java/TestClock.java
+++ src/test/java/TestClock.java
@@ -1,10 +1,17 @@
 import clock.Clock;
 import org.junit.jupiter.api.Test;
 import time.Time;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestClock {
     @Test
     void testCreateClock() {
         Clock clock = new Clock(new Time(5, 10));
+    }
+
+    @Test
+    void getClock(){
+        Clock clock = new Clock(new Time(5, 10));
+        assertEquals(new Time(5,10), clock.getTime());
     }
 }
