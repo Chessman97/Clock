9d5a3dcc3b7c5b08cffeebc75ca285ac7d0812ec
Chessman97 <47553161+Chessman97@users.noreply.github.com>
2019-04-01 12:54:42 +0600

    Тест на задание времент сигнала

--- src/test/java/TestClock.java
+++ src/test/java/TestClock.java
@@ -17,4 +17,11 @@ public class TestClock {
         Clock clock1 = new Clock(new Time(10, 11));
         assertEquals(new Time(10,11), clock1.getTime());
     }
+
+    @Test
+    void setSignalClock(){
+        Clock clock = new Clock(new Time(5, 10));
+        clock.setSignal(new Time(5, 0));
+
+    }
 }
