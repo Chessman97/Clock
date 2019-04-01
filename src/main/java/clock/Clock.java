a252d1a469e72289eac3fbc17a85d16b4b3f3c05
Chessman97 <47553161+Chessman97@users.noreply.github.com>
2019-04-01 12:56:33 +0600

    Тест на задание времени сигнала успешен!

--- src/main/java/clock/Clock.java
+++ src/main/java/clock/Clock.java
@@ -3,12 +3,18 @@ package clock;
 import time.Time;
 
 public class Clock {
+
     private Time time;
+    private Time signalTime;
     public Clock(Time time) {
         this.time = time;
     }
 
     public Time getTime() {
         return time;
+    }
+
+    public void setSignal(Time time) {
+        signalTime = time;
     }
 }
