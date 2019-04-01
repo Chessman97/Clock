187621e43322a82aa114a236a80446300a400661
Chessman97 <47553161+Chessman97@users.noreply.github.com>
2019-04-01 11:08:21 +0600

    Все тесты успешны!

--- src/test/java/TestTime.java
+++ src/test/java/TestTime.java
@@ -2,10 +2,6 @@ import org.junit.jupiter.api.Test;
 import time.Time;
 
 public class TestTime {
-    @Test
-    void testCreateTime() {
-        Time time = new Time();
-    }
 
     @Test
     void testCreateConcrateTime() {
